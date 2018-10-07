f4cb95831fd730a58bdc3af3f098e46c29c4ad1d
Eslam Amin <eslamamin@MacBook-Air-1742.local>
2018-10-07 13:18:39 -0500

    second commit

--- PoolTable.py
+++ PoolTable.py
@@ -1,5 +1,3 @@
-# Creating 12 table
-
 Pool_Tables = []
 for number in range(1,13):
   Pool_Tables.append(["Table_Number"] * 12)
@@ -7,65 +5,63 @@ for number in range(1,13):
 
 
 class PoolTable():
-    def __init__(self,Table_Number):
-        self.Table_Number = Table_Number
-        self.Table_Status = False
-        self.Check_in = None
-        self.Check_Out = None
-        self.Total_Time_Played = Total_Time_Played
-        self.Cost = Cost
-        self.User_Name = User_Name
+  def __init__(self,Table_Number):
+    self.Table_Number = Table_Number
+    self.Table_Status = False
+    self.Check_in = None
+    self.Check_Out = None
+    self.Total_Time_Played = Total_Time_Played
+    self.Cost = Cost
+    self.User_Name = User_Name
 
-    def __repr__(self):
-         return ("Welcome to Pool Table Game %s") % (self.User_Name)
+  def __repr__(self):
+    return ("Welcome to Pool Table Game %s") #% (self.User_Name)
 
-    def description(self):
-        print ("Welcome to the fun house %s") % (self.User_Name)
-        Print ("To start game please Enter s" or q to exit")
-
-
-
-    def starting_pool_table(self):
-        UserInput = input("Please Enter User Name: ")
-        UserInput = input("Please Enter Table_Number: ")
-        self.Table_Status = availability
-        if availability == False and User_Name ="":
-            print "This Table is open now "
-            print ("Welcome to Pool Table Game click c to continue")
-        else:
-            " This Table is Occupied  now please selet different table "
+  def description(self):
+    print ("Welcome to the fun house Pool Table Game")
 
 
 
-    def playing_game(self, Table_Num):
-        self.Table_Status = True
-        self.Check_In = datetime.datetime.now()
-        self.Check_Out= datetime.datetime.now()
-        Total_Time_Played = self.Check_Out - self.Check_In
-        Total_Time_Played = int(Total_Time_Played.total_seconds() * 1000)
+  def starting_pool_table(self):
+      UserInput = input("Please Enter User Name: ")
+      if UserInput == "":
+        print ("Welcome to Pool Table Game click proceed to start Game")
+      else:
+          print ("Please Enter Vaild Name")
+
+  def playing_game(self, Table_Num):
+    self.Table_Status = True
+    self.Check_In = datetime.datetime.now()
+    self.Check_Out= datetime.datetime.now()
+    Total_Time_Played = self.Check_Out - self.Check_In
+    Total_Time_Played = int(Total_Time_Played.total_seconds() * 1000)
     while True:
-        Table = Pool_Tables[Table_Num - 1]
+      Table = Pool_Tables[Table_Num - 1]
 
-    def ending_game(self, Check_out_Time):
-        userInput  = input("Press click C to check out")
-        self.Check_Out = Check_out_Time
-        self.Table_Status = "Open"
-    Print ("This Table is Open now")
+  def ending_game(self,Check_Out_Time):
+    userInput  = input("Press click C to check out")
+    Check_Out_Time = self.Check_Out
+    self.Table_Status = "Open"
+    print ("This Table is Open now")
 
-    def game_cost(self,Time):
-        Cost_Per_Hour == 3
-        self.cost = Game_Cost
-        self.Total_Time_Played = Time
-        if Time >= 1:
-            Game_Cost = Time * Cost_Per_Hour
-            return Game_Cost
-        else:
-            return float(Time) * Cost_Per_Hour
+  def game_cost(self):
+    Cost == 3
+    self.cost = Game_Cost
+    self.Total_Time_Played = Time
+    if Time >= 1:
+        Game_Cost = Time * Cost
+        return Game_Cost
+    else:
+        return float(Time) * Cost
 
 
+  def exit_game(self):
+    userinput  = input("Press q to exit")
+    print ("Thanks for playing with us Good Bye")
 
-        def exit_game(self):
-             userinput  = input ("Press q to exit")
-        print ("Thanks for playing with us Good Bye")
 
-Pool_Tables.description()
+pool_table_obj = PoolTable()
+pool_table_obj.description()
+pool_table_obj.starting_pool_table()
+pool_table_obj.ending_game()
+pool_table_obj.game_cost()
